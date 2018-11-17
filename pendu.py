766e8c7387274de9b1856bb0d16858e1cf4c4047
blingstand <blingstand@hotmail.fr>
2018-11-17 13:57:18 +0400

    création du fichier pendu.py qui import des modules de package, donnee.py rempli de 54 mots

---
+++ pendu.py
@@ -0,0 +1,25 @@
+"""fichier pendu.py qui contiendra notre jeu du pendu. """
+import package.donnees as pd
+import package.functions as pf
+import package.scores as ps
+import random
+
+
+print("\n", "* * "*30, "\nBienvenu dans le jeu du Pendu : Le joueur tente de trouver les lettres composant le mot. À chaque coup, il saisit une lettre. Si la lettre figure dans le mot, l'ordinateur affiche le mot avec les lettres déjà trouvées. Celles qui ne le sont pas encore sont remplacées par des étoiles (*). Le joueur a 8 chances. Au delà, il a perdu.")
+def main () : 
+#l'ordi demande au joueur d'enregistrer son nom, pour enregistrer le score
+
+#l'ordi choisit un mot parmis une liste, 
+
+#un mot de 8 lettres max
+
+# le joueur donne une lettre 
+
+# l'ordinateur vérifie
+
+
+#calcul du score = nombre de coups restants
+
+# a la fin écriture du score
+
+  pass
