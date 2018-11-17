1d0ba20e55aa7d37b9a00a5bbe5dc5b25239e84e
blingstand <blingstand@hotmail.fr>
2018-11-17 14:32:53 +0400

    ajout de la deuxième function choix_mot

--- package/functions.py
+++ package/functions.py
@@ -1,1 +1,17 @@
 """Le fichier fonctions.py qui contiendra les fonctions utiles à notre application. Là, je ne vous fais aucune liste claire, je vous conseille de bien y réfléchir, avec une feuille et un stylo si cela vous aide (Quelles sont les actions de mon programme ? Que puis-je mettre dans des fonctions ?)."""
+
+##--- import
+import package.donnees as pd
+import package.scores as ps
+from random import randint
+
+##--- functions
+#récupère le nom du joueur
+def name() : 
+  name = input("Entrez votre pseudo pour le jeu (ex : Blingstand) : ")
+  return name
+
+#sélectionne un mot de la liste
+def choix_mot() : 
+  mot = pd.liste_de_mots[randint(0, len(pd.liste_de_mots)-1)]
+  return mot
--- pendu.py
+++ pendu.py
@@ -2,14 +2,20 @@
 import package.donnees as pd
 import package.functions as pf
 import package.scores as ps
-import random
+
 
 
 print("\n", "* * "*30, "\nBienvenu dans le jeu du Pendu : Le joueur tente de trouver les lettres composant le mot. À chaque coup, il saisit une lettre. Si la lettre figure dans le mot, l'ordinateur affiche le mot avec les lettres déjà trouvées. Celles qui ne le sont pas encore sont remplacées par des étoiles (*). Le joueur a 8 chances. Au delà, il a perdu.")
+print("\n", "* * "*30,)
+
+
 def main () : 
 #l'ordi demande au joueur d'enregistrer son nom, pour enregistrer le score
+  name = pf.name()
+  print("Ok alors je vous appellerai : ", name)
 
-#l'ordi choisit un mot parmis une liste, 
+  mot = pf.choix_mot()
+  print("je vais vous faire trouver le mot : ", mot)
 
 #un mot de 8 lettres max
 
@@ -17,9 +23,8 @@ def main () :
 
 # l'ordinateur vérifie
 
-
 #calcul du score = nombre de coups restants
 
 # a la fin écriture du score
 
-  pass
+main()
